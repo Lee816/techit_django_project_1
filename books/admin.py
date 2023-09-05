74433f706eb8655e4e20c0d359cf5bdb0418abfe
Lee816 <lgha0816@naver.com>
2023-09-05 17:57:03 +0900

    books 앱 모델 생성

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Book, Books_rental
+
 # Register your models here.
+
+admin.site.register(Books_rental)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone, dates
+
+from accounts.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    category = models.ForeignKey(Category, related_name='books',on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+    stock = models.IntegerField(default=0)
+    summary = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+class Books_rental(models.Model):
+    rental_date = models.DateField(default=timezone.now())
+    return_date = models.DateField(default=timezone.now()+timezone.timedelta(weeks=1))
+    book = models.ForeignKey(Book, related_name='rentals', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='rental_books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} rental "{self.book}"'
